aaf065d7e1f6afae39d9fa5806d92dfb39f81b99
nnoyess <nnoyes17@gmail.com>
2026-02-04 13:38:49 -0500

    added decode

--- decoder/decoder.py
+++ decoder/decoder.py
@@ -7,6 +7,32 @@ binary = ["00000","00001","00010","00011","00100","00101","00110","00111","01000
 english =["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
 
 #Defs
+def inputer(text,type):
+  fail = True
+  msg = "Invaled"
+  while fail == True:
+    if typee == "bool":
+      try:
+        fail = False
+        x = bool(input(letters))
+      except ValueError:
+        print(msg)
+        
+    if typee == "float":
+      try:
+        fail = False
+        x = float(input(letters))
+      except ValueError:
+        print(msg)
+
+    if typee == "int":
+      try:
+        x = int(input(letters))
+        fail = False
+      except ValueError:
+        print(msg)
+  return x
+
 def retrieveBinary(fileName):
     try:
         with open(fileName, "r") as readFile:
@@ -32,15 +58,17 @@ def retrieveBinary(fileName):
             return False
 def decode(message,binary,english):
     for char in message:
-
+        print(9)
 
 def decode(message,binary,english):
+    decoded = ""
     for char in message:
         if char in binary:
-            binary[message.index(char)]
-        else:
-            print(False)
+            decoded += binary[message.index(char)]
 
+        else:
+            print("a letter failed to load")
+    return(decoded)
 #body
 run = True
 while run:
